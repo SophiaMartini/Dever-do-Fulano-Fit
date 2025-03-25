24f93ca5240d237a4ac415846457d0987dfb6c38
Sophia Martini de Albuquerque <sopmartinidea@gmail.com>
2025-03-25 17:19:26 -0300

    commit5

--- registro.js
+++ registro.js
@@ -1,16 +1,17 @@
+
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("registro");
 
     if (form) {
         form.addEventListener("submit", function (e) {
-            e.preventDefault(); 
+            e.preventDefault();
 
             let nome = document.getElementById("nome").value.trim();
             let email = document.getElementById("email").value.trim();
             let senha1 = document.getElementById("senha1").value.trim();
             let senha2 = document.getElementById("senha2").value.trim();
-            let peso = document.getElementById("peso").value.trim();
-            let altura = document.getElementById("altura").value.trim();
+            let peso = parseFloat(document.getElementById("peso").value.trim());
+            let altura = parseFloat(document.getElementById("altura").value.trim());
             let idade = parseInt(document.getElementById("idade").value.trim()) || 0;
             let sexo = document.getElementById("sexo").value.trim();
             let trh = document.getElementById("trh").value.trim();
@@ -37,19 +38,33 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("A senha deve possuir no mínimo 6 caracteres.");
                 return;
             }
+
+            if (isNaN(peso) || peso <= 0) {
+                alert("Insira um peso válido.");
+                return;
+            }
+
+            if (isNaN(altura) || altura <= 0) {
+                alert("Insira uma altura válida.");
+                return;
+            }
+
+            let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+
             let usuarioExistente = usuarios.find((user) => user.email === email);
             if (usuarioExistente) {
                 alert("Este e-mail já está cadastrado!");
                 return;
-}
-            let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
-            usuarios.push({ nome, email, senha: senha1, peso, altura, idade, sexo, trh, objetivo });
+            }
 
+            let novoUsuario = { nome, email, senha: senha1, peso, altura, idade, sexo, trh, objetivo };
+
+            usuarios.push(novoUsuario);
             localStorage.setItem("usuarios", JSON.stringify(usuarios));
+            localStorage.setItem("usuarioAtual", JSON.stringify(novoUsuario));
 
             alert("Cadastro realizado com sucesso!");
-
-            window.location.href = "home.html"; 
+            window.location.href = "home.html";
         });
     }
 });
